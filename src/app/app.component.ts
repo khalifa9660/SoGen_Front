cef13cd2849a656d8959e640672314fd1c2c7873
khali <asura.9660@gmail.com>
2023-12-01 19:32:51 +0100

    First Page SoGeneration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'SoGen_Front';
+export class AppComponent implements OnInit {
+  navHamburger: any;
+  navLinks: any;
+
+  constructor(private renderer: Renderer2) {}
+
+  title = "SoGen_Front";
+
+  ngOnInit() {
+    this.navHamburger = document.querySelector(".menu-hamburger");
+    this.navLinks = document.querySelector(".nav-links");
+  }
+
+  ngAfterViewInit() {
+    this.navHamburger.addEventListener("click", () => {
+      this.navLinks.classList.toggle("mobile-menu");
+    });
+  }
 }
