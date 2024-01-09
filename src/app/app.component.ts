e500483cfa8ec863497b110060beee089debfc59
khali <asura.9660@gmail.com>
2024-01-09 08:28:17 +0100

    Navbar finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Renderer2 } from "@angular/core";
+import { Component, OnInit, Renderer2, OnDestroy } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
 
 @Component({
   selector: "app-root",
@@ -8,8 +9,15 @@ import { Component, OnInit, Renderer2 } from "@angular/core";
 export class AppComponent implements OnInit {
   navHamburger: any;
   navLinks: any;
+  mobileQuery: MediaQueryList;
 
-  constructor(private renderer: Renderer2) {}
+  private _mobileQueryListener: () => void;
+
+  constructor(private media: MediaMatcher, private renderer: Renderer2) {
+    this.mobileQuery = media.matchMedia("(max-width: 900px)");
+    this._mobileQueryListener = () => {};
+    this.mobileQuery.addEventListener("change", this._mobileQueryListener);
+  }
 
   title = "SoGeneration";
 
@@ -22,5 +30,9 @@ export class AppComponent implements OnInit {
     this.navHamburger.addEventListener("click", () => {
       this.navLinks.classList.toggle("mobile-menu");
     });
+  }
+
+  ngOnDestroy() {
+    this.mobileQuery.removeEventListener("change", this._mobileQueryListener);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { LayoutModule } from "@angular/cdk/layout";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
